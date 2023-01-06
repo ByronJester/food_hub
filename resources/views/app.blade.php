058f644aa1f5d373cfdd22c378e81618a216e9a4
byronjester.manalo@gmail.com <byronjester.manalo@gmail.com>
2023-01-06 10:11:00 +0800

    m

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <link href="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.15.0/maps/maps.css" rel="stylesheet" type="text/css">
     <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.15.0/maps/maps-web.min.js" type="text/javascript"></script>
-    <script src="https://kit.fontawesome.com/4865c75cef.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/4cf4aca97a.js" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="{{ mix('/js/app.js') }}" defer></script>
 
